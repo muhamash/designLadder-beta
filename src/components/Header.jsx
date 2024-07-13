ad18f7e9bc2840055f3cf131251448bd6136527f
muhamash <muhammad-ashraful@outlook.com>
2024-07-13 21:49:48 +0600

    header left

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import DesignLadderButton from "../components//common/DesignLadderButton";
 export default function Header() {
     return (
         <div>
-            <div className="flex justify-center items-center w-[80%] gap-5 md:gap-10 mx-auto">
-                <div className="md:w-[50%]">
-                    <h1 className="text-[#172739] font-bold md:text-[70px] text-[50px] leading-tight">
+            <div className="flex md:flex-row flex-col-reverse justify-center items-center w-[90%] gap-5 md:gap-10 mx-auto">
+                <div className="md:w-[50%] container">
+                    <h1 className="text-[#172739] w-fit font-bold md:text-[70px] text-[45px] leading-tight pb-5">
                         Order a custom <span className="underline text-[#F06E16]">Website</span> like you would a pizza
                     </h1>
                     <DesignLadderButton text={ "Get Instant Quote" } className={"" } />
               </div>
-                <div className="md:w-[50%]">
+                <div className="md:w-[50%] container flex items-center justify-center">
                     no content
               </div>
             </div>
